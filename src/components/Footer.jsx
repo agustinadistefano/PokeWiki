3afe086d5cd1091a8f04c3fce268d6997f98fc6e
agustinadistefano <agustina.distefano@bue.edu.ar>
2025-03-16 22:14:55 -0300

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,19 @@
 export function Footer () {
 
     return (
-    <p className="footer">hola</p>
+    <>
+        <div className="footer">
+            <img 
+            className="logo-footer" 
+            type="image/svg+xml" 
+            src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/025.png"/> 
+            <h3> Agustina Isabel Distefano - UTN </h3> 
+            <img 
+            src="https://e7.pngegg.com/pngimages/173/464/png-clipart-pokemon-ball-pokeball-area-wiki-thumbnail.png" 
+            className="logo-footer"
+            alt="Pokeball"/>
+        </div>
+    </>
+    
     );
 }
